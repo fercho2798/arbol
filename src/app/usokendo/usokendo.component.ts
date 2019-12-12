62436aefc31add29da3d3140bd59e17d8b05bf89
John Doe <ferchofercho933@gmail.com>
2019-12-11 22:52:47 -0500

    agregar numeros

--- src/app/usokendo/usokendo.component.ts
+++ src/app/usokendo/usokendo.component.ts
@@ -6,21 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usokendo.component.css']
 })
 export class UsokendoComponent implements OnInit {
-  
-  public status = '#10b507';
-  public buttons = [
-    { text: 'Away', icon: 'k-i-clock', color: '#f0c505' },
-    { text: 'Available', icon: 'k-i-check-circle', color: '#10b507', selected: true },
-    { text: 'Offline', icon: 'k-i-close-circle', color: '#707070' },
-    { text: 'Do not disturb', icon: 'k-i-minus-circle', color: '#e30000' }
-  ];
-  private img = 'https://demos.telerik.com/kendo-ui/content/web/panelbar/nancy.jpg';
 
-  public selectedChange(e, btn) {
-    this.status = btn.color;
-  }
   constructor() { }
+eliminar() {
+  var cl= document.getElementById("cl");
+ document.getElementById("top").innerHTML = cl.value;
 
+}
   ngOnInit() {
   }
 
